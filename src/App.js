d7045defeffa35a77700178f5aa904ea02e6db43
Victoria Rouser <vic86rous@gmail.com>
2022-04-12 20:58:08 -0500

    finished functioning components

--- src/App.js
+++ src/App.js
@@ -18,13 +18,14 @@ const movie1 = {
 
 const App = () => {
 
-    const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -38,21 +39,30 @@ const App = () => {
             <div className="search">
                 <input
                 placeholder="Search for Movies"
-                value="The Avengers"
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                 src={SearchIcon}
                 alt="Search"
-                onClick={() => {}}
+                onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            <div className="container">
-            <MovieCard movie1={movie1} />
-            </div>
-        </div>
-    );
-}
+            {movies.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) =>  (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                    ) : (
+                        <div className="empty">
+                            <h2>No Movies Found</h2>
+                        </div>
+                    )}
+                </div>
+            );
+        }
 
 export default App;
